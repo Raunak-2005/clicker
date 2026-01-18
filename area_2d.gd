f9603fe1107c1ff40234a29e1c62db601ce5b76b
GreensBFoot <aryan.sahasrabudhe84@gmail.com>
2026-01-18 15:29:53 +0530

    xx

---
+++ area_2d.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		get_parent().animation="default"
+		get_parent().play()
+		get_parent().get_node("AudioStreamPlayer2D").play() 
+	pass # Replace with function body.
