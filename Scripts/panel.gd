f91b58d45c4544dedc8cbc58e1e6c89e2ad0fd52
Raunak-2005 <raunakthanawala@gmail.com>
2026-01-30 19:39:30 +0530

    with ip+ui

--- Scripts/panel.gd
+++ Scripts/panel.gd
@@ -1,6 +1,8 @@
 extends Panel
 signal startedserver
 signal startedclient
+@onready var line_edit: LineEdit = $"../LineEdit"
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -20,7 +22,7 @@ func _on_button_pressed() -> void:
 
 
 func _on_button_2_pressed() -> void:
-	MultiplayerManager1.create_client()
+	MultiplayerManager1.create_client($"../LineEdit".text)
 	startedclient.emit()
 	$Camera2D.enabled=false 
 	get_tree().root.get_node("Node2D/CameraCar").enabled=true
