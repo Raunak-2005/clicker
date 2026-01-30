c3431d6d9733a443c6f51586939e274fe67bcf12
Raunak-2005 <raunakthanawala@gmail.com>
2026-01-30 19:33:03 +0530

    Shop UI

---
+++ Scenes/car_shop.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+@export var costgirl = 10
+var costparrot = 10
+var coststick = 5
+var costtruck = 15
+var costoil = 20
+@onready var button1: Button = $Item1/Button
+@onready var button2: Button = $Item2/Button
+@onready var button3: Button = $Item3/Button
+@onready var button4: Button = $Item4/Button
+@onready var button5: Button = $Item5/Button
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed_girl() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed_parrot() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed_stick() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed_truck() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed_oil() -> void:
+	pass # Replace with function body.
